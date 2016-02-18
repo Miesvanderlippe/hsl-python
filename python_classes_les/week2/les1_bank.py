f319a234e149fea649622f87ca31c41f6e39e927
Miesvanderlippe <miesvanderlippe@gmail.com>
2016-02-18 12:35:13 +0100

    Added some bad failsafes

--- python_classes_les/week2/les1_bank.py
+++ python_classes_les/week2/les1_bank.py
@@ -8,16 +8,24 @@ class BankAccount:
         return 'Balance: {}'.format(self.balance)
 
     def set_balance(self, new_balance)->None:
-        self.balance = new_balance
+        self.balance = int_or_zero(new_balance)
 
     def get_balance(self)->int:
         return self.balance
 
     def deposit(self, amount: int)->None:
-        self.balance += amount
+        self.balance += int_or_zero(amount)
 
     def withdraw(self, amount: int)->None:
-        self.balance -= amount
+        self.balance -= int_or_zero(amount)
+
+
+def int_or_zero(presumed_int)->int:
+    try:
+        presumed_int = int(presumed_int)
+        return presumed_int
+    except ValueError:
+        return 0
 
 
 def main()->None:
