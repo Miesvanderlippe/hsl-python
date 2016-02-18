4d3fb43ea8455f735a3ae796e1fb98229356166d
Miesvanderlippe <miesvanderlippe@gmail.com>
2016-02-18 12:28:16 +0100

    Example 2, bank accounts

---
+++ python_classes_les/week2/les1_bank.py
@@ -0,0 +1,24 @@
+
+class BankAccount:
+
+    def __init__(self, starting_balance: int)->None:
+        self.balance = starting_balance
+
+    def __str__(self)->str:
+        return 'Balance: {}'.format(self.balance)
+
+    def set_balance(self, new_balance)->None:
+        self.balance = new_balance
+
+    def get_balance(self)->int:
+        return self.balance
+
+    def deposit(self, amount: int)->None:
+        self.balance += amount
+
+
+def main()->None:
+    pass
+
+if __name__ == '__main__':
+    main()
