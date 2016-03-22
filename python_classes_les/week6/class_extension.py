2bd40e1c5cc5bd727e038bf3191ee774442305f8
Miesvanderlippe <miesvanderlippe@gmail.com>
2016-03-22 12:21:28 +0100

    Added circle shape

--- python_classes_les/week6/class_extension.py
+++ python_classes_les/week6/class_extension.py
@@ -1,3 +1,4 @@
+from math import pi
 
 class Rectangle:
 
@@ -35,15 +36,38 @@ class Square:
         return self.height * self.width
 
 
+class Circle:
+
+    radius = int
+
+    def __init__(self, radius: int)->None:
+        self.radius = radius
+
+    @property
+    def perimeter(self)->int:
+        return self.radius * pi * 2
+
+    @property
+    def surface(self)->int:
+        return (self.radius * self.radius) * pi
+
+
 def main()->None:
+
+    print(pi)
+
     shape1 = Rectangle(100, 200)
     shape2 = Square(100)
+    shape3 = Circle(100)
 
     print('Shape1 surface: {}'.format(shape1.surface))
     print('Shape1 perimiter: {}'.format(shape1.perimeter))
 
     print('Shape2 surface: {}'.format(shape2.surface))
     print('Shape2 perimiter: {}'.format(shape2.perimeter))
+
+    print('Shape3 surface: {}'.format(shape3.surface))
+    print('Shape3 perimiter: {}'.format(shape3.perimeter))
 
 if __name__ == '__main__':
     main()
